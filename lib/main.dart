b875c83594f6bb80a9839f1ed727d605a67a60b9
Su Hlaing Thin <115348141+suhiaing@users.noreply.github.com>
2024-03-08 17:22:27 +0630

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: SizedBox(
@@ -19,8 +21,14 @@ class MyApp extends StatelessWidget {
               children: [
                 const Text("Welcome to my converter app!",
                     style:
-                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
+                        TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 50),
+                const Text("' 0 '",
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 50,
+                ),
                 TextField(
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
@@ -29,8 +37,20 @@ class MyApp extends StatelessWidget {
                           borderRadius: BorderRadius.circular(5)),
                       hintText: "Add the amount you want to convert"),
                 ),
-                const SizedBox(height: 20),
-                TextButton(onPressed: () {}, child: const Text("Click"))
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (kDebugMode) {
+                        print("Clicked");
+                      }
+                    },
+                    style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                        foregroundColor: MaterialStatePropertyAll(Colors.white),
+                        minimumSize: MaterialStatePropertyAll(Size(130, 50))),
+                    child: const Text("Click"))
               ],
             ),
           ),
